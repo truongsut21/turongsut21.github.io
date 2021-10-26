d9b4158845cfc21a1d1a002c3873626ff29b952f
truongsut21 <dotruong0704@gmail.com>
2021-10-26 07:42:35 +0700

    main

--- main.js
+++ main.js
@@ -1,31 +1,27 @@
 // document.getElementById("au").autoplay;
 
-alert("chuc em yêu vui vẻ nà :3");
-setTimeout(function () {
-  document.getElementById("cl1").setAttribute("class", "btnClick1");
-}, 3000);
+alert("chuc em yêu vui vẻ nà :3")
+setTimeout(function(){document.getElementById('cl1').setAttribute('class', 'btnClick1')}, 3000);
 
 function clickStar() {
-  alert(
-    "Anh có điều này muốn nói với em... thật ra anh thích từ lâu lắm rồi, anh cũng rất muốn trở thành người yêu của em, mãi cố gắng phấn đấu dành cho em điều hp nhất"
-  );
-  document.getElementById("cl1").setAttribute("class", "none");
-  document.getElementById("cl2").setAttribute("class", " ");
+    alert("Anh có điều này muốn nói với em... thật ra anh thích từ lâu lắm rồi, anh cũng rất muốn trở thành người yêu của em, mãi cố gắng phấn đấu dành cho em điều hp nhất");
+    document.getElementById('cl1').setAttribute("class", "none");
+    document.getElementById('cl2').setAttribute('class', ' ');
+
 }
 var tyLe = 10;
 var x = 0;
 function khongDongY() {
-  x = x + 50;
-  var y = Math.random() * 10 + 30;
-  console.log(x);
-  document
-    .getElementById("no")
-    .setAttribute("style", "margin-top: " + x + "px");
+    x = x + 50;
+    var y = Math.random() * 10 + 30;
+    console.log(x)
+    document.getElementById('no').setAttribute("style", "margin-top: " + x + "px");
 }
 function yes() {
-  tyLe = tyLe + 10;
-  document.getElementById("phanTram").innerHTML = "em chắc chứ " + tyLe + " %";
-  if (tyLe == 100) {
-    window.location.assign("CauHoi/indexCauHoi.html");
-  }
+ tyLe = tyLe +10;
+ document.getElementById('phanTram').innerHTML = "em chắc chứ " + tyLe + " %";
+ if(tyLe == 100) {
+    window.location.assign("CauHoi/indexCauHoi.html")
+ }
+ 
 }
